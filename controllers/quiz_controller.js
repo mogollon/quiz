1e74112de4f9456274cfbd17ed29cd4ffb999d08
mogollon <mogo2k@gmail.com>
2015-06-18 20:16:45 +0200

    sequelize + sqlite3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,17 @@
+var models = require('../models/models.js');
+
 exports.question = function(req,res) {
-    res.render('quizes/question', {pregunta: 'Capital de Italia'})
+    models.Quiz.findAll().then(function(quiz) {
+        res.render('quizes/question', {pregunta: 'Capital de Italia'});
+    });
 };
 
 exports.answer = function(req,res) {
-    if(req.query.respuesta === 'Roma') {
-        res.render('quizes/answer', {respuesta: 'Correcta'});
-    } else {
-        res.render('quizes/answer', {respuesta: 'Incorrecta'});
-    }
+    models.Quiz.findAll().then(function(quiz) {
+        if(req.query.respuesta === quiz[0].respuesta) {
+            res.render('quizes/answer', {respuesta: 'Correcta'});
+        } else {
+            res.render('quizes/answer', {respuesta: 'Incorrecta'});
+        }
+    });
 };
